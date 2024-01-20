dec9aa118615127cfa3926540112f952daff3d40
Rahul Meena <rahulmeena626441@gmail.com>
2024-01-21 00:34:49 +0530

    Your commit message here

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app/model/meal.dart';
+import 'package:meal_app/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key,required this.meal});
+  const MealItem({super.key, required this.meal});
 
- final Meal meal;
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase()+meal.complexity.name.substring(1);
+  }
+
+    String get affordibilityText {
+    return meal.affordability.name[0].toUpperCase()+meal.affordability.name.substring(1);
+  }
+
+  final Meal meal;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -14,44 +23,60 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-      onTap: (){},
-      child: Stack(
-        children: [ 
-           FadeInImage(placeholder: MemoryImage(kTransparentImage), 
-          image: NetworkImage(meal.imageUrl),
-          fit: BoxFit.cover,
-          height: 200,
-          width: double.infinity,
-          ),
-          Positioned(
-            bottom: 0,
-            right: 0,
-            left: 0,
-            child: Container(
-              color: Colors.black54,
-              padding: const EdgeInsets.symmetric(vertical: 6,horizontal: 44),
-              child: Column(children: [
-                Text(meal.title,maxLines: 2,
-                textAlign: TextAlign.center,
-                softWrap: true
-                 ,overflow: TextOverflow.ellipsis, // this overflow show ... if title is very big,
-                 style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white
-                 ),
-                 ),
-               const SizedBox(height: 12),
-                
-                Row(
+        onTap: () {},
+        child: Stack(
+          children: [
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
+            ),
+            Positioned(
+                bottom: 0,
+                right: 0,
+                left: 0,
+                child: Container(
+                  color: Colors.black54,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 6, horizontal: 44),
+                  child: Column(children: [
+                    Text(
+                      meal.title, maxLines: 2,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow
+                          .ellipsis, // this overflow show ... if title is very big,
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    const SizedBox(height: 12),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealIteamTrait(
+                            icon: Icons.schedule, label: '${meal.duration}'),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        MealIteamTrait(
+                            icon: Icons.work, label: complexityText),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        MealIteamTrait(
+                            icon: Icons.attach_money, label: affordibilityText),
 
-                )
-
-              ]),
-            ) 
-          )
-        ],
+                      ],
+                    )
+                  ]),
+                ))
+          ],
+        ),
       ),
-    ),);
+    );
   }
 }
