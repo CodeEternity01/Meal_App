dd509cb9a256531b200747b699f3346998584238
rahul_1.21 <rahulmeena626441@gmail.com>
2024-02-08 01:57:10 +0530

    Filters are working quite well

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,38 +6,43 @@ import 'package:meal_app/widgets/categories_grid_item.dart';
 import 'package:meal_app/model/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key,required this.ontoggleFavorites});
+  const CategoriesScreen({super.key, required this.ontoggleFavorites,required this.avialableM});
 
-    final void Function(Meal meals) ontoggleFavorites;
+  final void Function(Meal meals) ontoggleFavorites;
+  final List<Meal> avialableM;
   void _selectCategory(BuildContext context, Category category) {
-   final filteredMeals= avialableMeals.where((meal) => meal.categories.contains(category.id)).toList();
+    final filteredMeals = avialableM
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
     // Navigator(context,route);
     Navigator.of(context).push(MaterialPageRoute(
-        builder: (cxt) =>
-             MealsScreen(title: category.title, meals: filteredMeals,ontoggleFavorites: ontoggleFavorites,)));
+        builder: (cxt) => MealsScreen(
+              title: category.title,
+              meals: filteredMeals,
+              ontoggleFavorites: ontoggleFavorites,
+            )));
   }
 
   @override
   Widget build(BuildContext context) {
     return GridView(
-        
-        padding: const EdgeInsets.all(10),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: [
-          // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
-          for (final category in availableCategories)
-            CategoryGridItem(
-              category: category,
-              onSelectCategory: () {
-                _selectCategory(context,category);
-              },
-            )
-        ],
+      padding: const EdgeInsets.all(10),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: [
+        // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectCategory: () {
+              _selectCategory(context, category);
+            },
+          )
+      ],
     );
   }
 }
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/data/dummy_data.dart';
 import 'package:meal_app/screens/categories.dart';
 import 'package:meal_app/screens/filter.dart';
 import 'package:meal_app/screens/meals.dart';
 import 'package:meal_app/model/meal.dart';
 import 'package:meal_app/widgets/main_drawer.dart';
+
+const kInitialFilter = {
+  Filter.glutenFree: false,
+  Filter.lactoseFree: false,
+  Filter.vegan: false,
+  Filter.vegetarian: false,
+};
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -18,12 +26,7 @@ class _TabsScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
 
   final List<Meal> _favorites = [];
-   Map<Filter, bool> _selectedFilter={
-    Filter.glutenFree=false,
-    Filter.lactoseFree=false,
-    Filter.vegan=false,
-    Filter.vegetarian=false,
-  }
+  Map<Filter, bool> _selectedFilter = kInitialFilter;
 
   void _showInfoMessage(String message) {
     ScaffoldMessenger.of(context).clearSnackBars();
@@ -53,10 +56,9 @@ class _TabsScreenState extends State<TabsScreen> {
     if (identifier == 'Filters') {
       final result = await Navigator.of(context).push<Map<Filter, bool>>(
           MaterialPageRoute(builder: (cxt) => const FilterScreen()));
-          setState(() {
-            _selectedFilter=result??;
-          });
-     
+      setState(() {
+        _selectedFilter = result ?? kInitialFilter;
+      });
     } else if (identifier == 'Favorites') {
       // here it an issue to solve that when ever favorites clicked by drawer than screen got stack
       Navigator.of(context).push(MaterialPageRoute(
@@ -75,8 +77,24 @@ class _TabsScreenState extends State<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final availableM = avialableMeals.where((meal) {
+      if (_selectedFilter[Filter.glutenFree]! && !meal.isGlutenFree) {
+        return false;
+      }
+      if (_selectedFilter[Filter.lactoseFree]! && !meal.isLactoseFree) {
+        return false;
+      }
+      if (_selectedFilter[Filter.vegan]! && !meal.isVegan) {
+        return false;
+      }
+      if (_selectedFilter[Filter.vegetarian]! && !meal.isVegetarian) {
+        return false;
+      }
+      return true;
+    }).toList();
     Widget activePage = CategoriesScreen(
       ontoggleFavorites: _toggleMealFavoritieStatus,
+      avialableM: availableM,
     );
     var activePageTitle = 'Categories';
 
