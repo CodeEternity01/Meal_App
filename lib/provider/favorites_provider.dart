08b9d1e71191fd116984c5c52274c19a316a96a7
rahul_1.21 <rahulmeena626441@gmail.com>
2024-02-12 13:37:45 +0530

    stateNotifier and way in it how to replace the list and manage

---
+++ lib/provider/favorites_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meal_app/model/meal.dart';
+
+class FavoritesMealNotifier extends StateNotifier<List<Meal>> {
+  FavoritesMealNotifier() : super([]);
+
+  void toggleMealFavoritesStatus(Meal meal) {
+    final isMealFavorites = state.contains(meal);
+
+    if (isMealFavorites) {
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+final favoriteMealProvider = StateNotifierProvider();
